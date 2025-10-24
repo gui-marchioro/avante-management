886b4b88dcb259461dcd58e018d700ef7dfd3a10
Guilherme Marchioro <gui02_marchioro@hotmail.com>
2025-10-24 18:59:53 -0300

    Added groups to the application within group validation logic

--- src/users/apps.py
+++ src/users/apps.py
@@ -1,6 +1,19 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+from . import groups
+
+
+def create_default_groups(sender, **kwargs):
+    from django.contrib.auth.models import Group
+    for name in [groups.DEFAULT, groups.MANAGER]:
+        Group.objects.get_or_create(name=name)
 
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+    def ready(self):
+        # Auto create user groups
+        # Import here to avoid crash due to app registry not ready
+        post_migrate.connect(create_default_groups, sender=self)
--- src/users/views.py
+++ src/users/views.py
@@ -1,13 +1,18 @@
+from django.contrib.auth.models import Group, User
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from . import groups
 
 
 def register(request: HttpRequest) -> HttpResponse:
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
+            user: User = form.save()
+            group = Group.objects.get(name=groups.DEFAULT)
+            user.groups.add(group)
+            user.save()
             return redirect('users:login')
     else:
         form = RegisterForm()
